a142874bce279dd3e3f06e204630753741cd2870
wictor-parmenis <wictortec@gmail.com>
2021-09-16 19:52:45 -0300

    adjusts in code.

--- src/main/java/com/wictortec/crm/ApiclientsApplication.java
+++ src/main/java/com/wictortec/crm/ApiclientsApplication.java
@@ -2,12 +2,15 @@ package com.wictortec.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApiclientsApplication {
+public class ApiclientsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiclientsApplication.class, args);
 	}
+	
+
 
 }
